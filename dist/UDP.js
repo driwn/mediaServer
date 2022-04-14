4c49e42927c6b55418fc84c2d4b69cc543516f12
Иван <dragunovskiyiwn@yandex.ru>
2022-04-14 18:18:29 +0200

    itter add

--- dist/UDP.js
+++ dist/UDP.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startUDPserver = void 0;
 const dgram_1 = __importDefault(require("dgram"));
-const startUDPserver = () => {
+const connect = (num, resolve, reject) => {
     const client = dgram_1.default.createSocket('udp4');
     client.on('error', (err) => {
         console.log(`UDP server error:\n${err.stack}`);
         client.close();
+        reject();
     });
     client.on('message', (msg, rinfo) => {
         console.log(`UDP server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
@@ -17,9 +18,13 @@ const startUDPserver = () => {
     client.on('connect', () => {
         const address = client.address();
         console.log(`UDP server listening ${address.address}:${address.port}`);
+        resolve(client);
     });
-    client.bind(744, '10.0.0.1');
-    return client;
+    client.bind(744, `10.0.0.${num}`);
 };
-exports.startUDPserver = startUDPserver;
+exports.startUDPserver = new Promise((resolve, reject) => {
+    for (let n = 1; n < 32; n++) {
+        connect(n, resolve, reject);
+    }
+});
 //# sourceMappingURL=UDP.js.map
--- src/UDP.ts
+++ src/UDP.ts
@@ -1,11 +1,12 @@
 import dgram from 'dgram'
 
-export const startUDPserver = () => {
+const connect = (num: number, resolve: any, reject: any) => {
     const client = dgram.createSocket('udp4')
 
     client.on('error', (err) => {
         console.log(`UDP server error:\n${err.stack}`)
         client.close()
+        reject()
     })
 
     client.on('message', (msg, rinfo) => {
@@ -17,9 +18,13 @@ export const startUDPserver = () => {
     client.on('connect', () => {
         const address = client.address()
         console.log(`UDP server listening ${address.address}:${address.port}`)
+        resolve(client)
     })
 
-    client.bind(744, '10.0.0.1')
-
-    return client
+    client.bind(744, `10.0.0.${num}`)
 }
+export const startUDPserver = new Promise<dgram.Socket>((resolve, reject) => {
+    for (let n = 1; n < 32; n++) {
+        connect(n, resolve, reject)
+    }
+})
